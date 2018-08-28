98b86fb27549af2e0dfa726252a084fe0c030e1a
Lemuel G. Enrico <lenrico@openit.com>
2018-08-28 10:19:40 +0800

    first backend commit

---
+++ company.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>ROI Calculator</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+      <nav class="navbar navbar-default nobord const navsmallmrgbot">
+        <div class="container-fluid blueb">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+              <img src="images/Snowlogo.png"></a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><h class="whitfnt">Snow for Engineering: ROI Calculator</p></a></li>
+              <li><a href="help.html"><h class="whitfnt2">Help</p></a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+      <div class="jumbotron">
+        <div class="row">
+          <div class="col-md-6 col-sm-6 centertext">
+            <h2 class="headngsprpg">Company Details</h2>
+            <br>
+            Select you Industy
+            <br>
+            <br>
+            <form>
+              <!-- delete under if done -->
+              <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary">
+                  <input type="radio" name="options" id="option1" autocomplete="off">Engineering
+                </label>
+                <label class="btn btn-secondary">
+                  <input type="radio" name="options" id="option2" autocomplete="off">Oil and Gas
+                </label>
+                <label class="btn btn-secondary">
+                  <input type="radio" name="options" id="option3" autocomplete="off">Automotive
+                </label>
+                <br>
+                <label class="btn btn-secondary">
+                  <input type="radio" name="options" id="option1" autocomplete="off">Mechanical & Precision
+                </label>
+                <label class="btn btn-secondary">
+                  <input type="radio" name="options" id="option2" autocomplete="off">Government
+                </label>
+                <label class="btn btn-secondary">
+                  <input type="radio" name="options" id="option3" autocomplete="off">Others
+                </label>
+              </div>
+
+
+            </form>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="rightalgn">
+              <button onclick="nopdf()" class="btn btn-warning"><!--<span class="glyphicon glyphicon-download-alt">--> Download PDF</button>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="leftdiv">
+              <br>
+              <br>
+              <br>
+
+              <form>
+                <div class="form-group">
+                  <input type="text" class="form-control input-lg" placeholder="Company Name" required>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control input-lg" placeholder="Website" required>
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control input-lg" placeholder="Contact Email" required>
+                </div>
+                <div class="form-group">
+                  <input type="number" class="form-control input-lg" placeholder="Number of Users" required>
+                </div>
+                <div class="notssub"><strong>Note:</strong> All fields are required.</div>
+                <div class="rightalgn">
+                <button type="submit" class="nextbutdes" formaction="software.html">Next &raquo</button>
+                </div>
+              </form>
+              <br>
+            </div>
+          </div>
+        </div><!--row-->
+
+      </div>
+
+      <!--will delete this later -->
+    <script>
+      function nopdf() {
+        alert("We are working on this feature.");
+        }
+    </script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <p class="dfooter">Copyright © ROI Calculator. All rights reserved.</p>
+  </body>
+</html>
