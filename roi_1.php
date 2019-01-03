82615a42632b6cd1b18dc7a5cf835809cbb52820
Lemuel G. Enrico <lenrico@openit.com>
2019-01-03 22:08:40 +0800

    proofread on w/cost

--- roi_1.php
+++ roi_1.php
@@ -476,7 +476,7 @@
                 <div id="analline">
 
                 <div class="leftalgn openfont whitefont"><br>
-                <p> <span class="analysishighlight">&#9632;</span> The value of ROI is already noticeable on the first year of purchase since a lot of optimization will happen on this period.
+                <p> <span class="analysishighlight">&#9632;</span> The ROI is already noticeable in the first year of purchase since a lot of optimization will happen on this period.
                     That's why it is observable that ROI for the following years is much lower than the first year of purchase.<br>
                     <span class="analysishighlight">&#9632;</span> For the following years, the ROI will be relative to the <span class="analysishighlight">Support and Maintenance cost</span> that is <span class="analysishighlight">20%</span> of Snow for Engineering purchase cost.</p>
                     <a target="_blank" href="https://www.snowsoftware.com/int/contact">Ask a Snow for Engineering expert &raquo;</a><br><br>
